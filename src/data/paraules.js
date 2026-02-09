dc477acb92369f1118dd832e77ca7cda336e7227
1DAMSCojita <scojita20207@iesjoanramis.org>
2026-02-09 19:50:22 +0100

    primer commit

---
+++ src/data/paraules.js
@@ -0,0 +1,9 @@
+
+// Creamos un array de palabras en catalán:
+export const paraules = [
+    'CONSOLA',
+    'TECLAT',
+    'GRAFICA',
+    'NIVELLS',
+    'MISSIONS'
+];
---
+++ src/data/preguntes.js
@@ -0,0 +1,29 @@
+
+// Creamos un array con preguntas:
+export const preguntes = [
+    {
+        pregunta: "Quin any es va llançar PlayStation?",
+        opcions: ["1992", "1994", "1996", "1998"],
+        resposta: "1994"
+    },
+    {
+        pregunta: "Quina empresa va crear Mario?",
+        opcions: ["Sony", "Microsoft", "Nintendo", "Sega"],
+        resposta: "Nintendo"
+    },
+    {
+        pregunta: "Quin és el joc més reconegut?",
+        opcions: ["Minecraft", "Grand Theft Auto V", "Super Mario Bros: 1", "Metal Gear Solid II"],
+        resposta: "Minecraft"
+    },
+    {
+        pregunta: "Quina d'aquestes plataformes NO s'ha de pagar online?",
+        opcions: ["PlayStation", "Xbox", "Nintendo", "Steam"],
+        resposta: "Steam"
+    },
+    {
+        pregunta: "Quin joc d'aquests pertany com un exclusiu de PlayStation?",
+        opcions: ["God of War: Ragnarok", "Flight Simulator", "Sea of Thieves", "Halo: Infinite"],
+        resposta: "God of War: Ragnarok"
+    }
+]
