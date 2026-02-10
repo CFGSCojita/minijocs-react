814c7c7329b147e400509b469aedb988f4f7aaa5
1DAMSCojita <scojita20207@iesjoanramis.org>
2026-02-10 17:44:35 +0100

    Estructura de los componentes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import './App.css'
+// Importamos funciones necesarias de React Router DOM:
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// Importamos componentes:
+import Home from './components/Home'
+import JocParaules from './components/JocParaules'
+import Questionari from './components/Questionari'
+import ResultatFinal from './components/ResultatFinal'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/joc-paraules" element={<JocParaules />} />
+          <Route path="/questionari" element={<Questionari />} />
+          <Route path="/resultat" element={<ResultatFinal />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
