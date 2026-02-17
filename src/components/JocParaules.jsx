bf102bc33fd12d30319d0d3f9113f9e94929eb52
1DAMSCojita <scojita20207@iesjoanramis.org>
2026-02-17 17:45:27 +0100

    Questionari y refactorización de variables.

--- src/components/JocParaules.jsx
+++ src/components/JocParaules.jsx
@@ -9,43 +9,43 @@ export default function JocParaules() {
   const navigate = useNavigate(); // Declaramos un hook para poder navegar.
 
   // Declaración de variables de estado:
-  const [intentosRestantes, setIntentosRestantes] = useState(3);
-  const [puntos, setPuntos] = useState(0);
-  const [indicePalabra, setIndicePalabra] = useState(0);
+  const [intentsRestants, setIntentosRestantes] = useState(3);
+  const [punts, setPunts] = useState(0);
+  const [indexParaula, setIndexParaula] = useState(0);
   const [input, setInput] = useState('');
 
   // Creamos una función para comprobar el resultado del input y manejar la lógica del juego de palabras:
   function comprovar() {
     // Estructura de control 'if'.
     // Si la longitud del input no coincide con la longitud de la palabra correcta, no hacemos nada.
-    if (input.length !== paraules[indicePalabra].length) {
+    if (input.length !== paraules[indexParaula].length) {
       return;
     }
 
     // Estructura de control 'if'.
     // Si la longitud es válida (pueden ser minúsculas o mayúsculas), compararemos el input convertido con la palabra correcta, cuando la respuesta es correcta,
     // sumamos puntos. En caso contrario, restamos intentos:
-    if (input.toUpperCase() === paraules[indicePalabra]) {
-      setPuntos(puntos + 2); // Incrementamos dos puntos al estar correcta.
+    if (input.toUpperCase() === paraules[indexParaula]) {
+      setPunts(punts + 2); // Incrementamos dos puntos al estar correcta.
       
       // Estructura de control 'if'.
       // Verificamos que haya más palabras disponibles comparando el índice con la longitud. Si existen más palabras, avanzaremos a las siguientes, si no, guardamos
       // la puntuación en un localStorage:
-      if (indicePalabra + 1 < paraules.length) {
-        setIndicePalabra(indicePalabra + 1); // Incrementamos el índice.
+      if (indexParaula + 1 < paraules.length) {
+        setIndexParaula(indexParaula + 1); // Incrementamos el índice.
         setIntentosRestantes(3); // Reiniciamos los intentos a 3.
         setInput(''); // Limpiamos el campo de input.
       } else {
-        localStorage.setItem('punts', puntos + 2); // Guardamos la puntuación usando localStorage.
+        localStorage.setItem('punts', punts + 2); // Guardamos la puntuación usando localStorage.
         navigate('/resultat'); // Navegamos a la página de resultados.
       }
     } else {
-      setIntentosRestantes(intentosRestantes - 1); // Decrementamos un intento.
+      setIntentosRestantes(intentsRestants - 1); // Decrementamos un intento.
 
       // Estructura de control 'if'.
       // Si no quedan intentos, guardamos los puntos que el usuario haya conseguido en el localStorage y navegamos a la página de resultados.
-      if (intentosRestantes === 1) {
-        localStorage.setItem('punts', puntos);
+      if (intentsRestants === 1) {
+        localStorage.setItem('punts', punts);
         navigate('/resultat');
       }
       setInput(''); // Limpiamos el campo de input.
@@ -54,15 +54,15 @@ export default function JocParaules() {
 
   return (
     <div>
-      <p>{'_ ' .repeat(paraules[indicePalabra].length)}</p>
+      <p>{'_ ' .repeat(paraules[indexParaula].length)}</p>
       <input 
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)} 
       />
       <button onClick={comprovar}>Comprovar Resposta</button>
-      <p>Intents restants: {intentosRestantes}</p>
-      <p>Punts: {puntos}</p>
+      <p>Intents restants: {intentsRestants}</p>
+      <p>Punts: {punts}</p>
     </div>
   )
 }
--- src/components/Questionari.jsx
+++ src/components/Questionari.jsx
@@ -1,6 +1,48 @@
+// Importaciones necesarias:
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { preguntes } from "../data/preguntes"
 
 export default function Questionari() {
+  
+  const navigate = useNavigate(); // Creamos el hook para navegar entre rutas.
+
+  // Declaración de variables de estado:
+  const [indexPregunta, setIndexPregunta] = useState(0);
+  const [punts, setPunts] = useState(0);
+
+  const preguntaActual = preguntes[indexPregunta]; // Obtenemos la pregunta actual a mostrar según el índice.
+
+  // Creamos una función para seleccionar una opción y actualizar los puntos y el índice de la pregunta:
+  function seleccionarOpcio(opcio) {
+
+    // Declaramos una variable para calcular los nuevos puntos, sumando 2 si la opción seleccionada es la correcta:
+    const nousPunts = opcio == preguntaActual.resposta ? punts + 2 : punts;
+    
+    // Estructura de control 'if'.
+    // Si el índice de la pregunta actual más uno es menor que la longitud total de preguntas, actualizamos los puntos y el índice para mostrar la siguiente pregunta.
+    // En caso contrario, guardamos los puntos en el almacenamiento local y navegamos a la página de resultados.
+    if (indexPregunta + 1 < preguntes.length) {
+      setPunts(nousPunts); // Actualizamos los puntos.
+      setIndexPregunta(indexPregunta + 1); // Actualizamos el índice para mostrar la siguiente pregunta.
+    } else {
+      localStorage.setItem('punts', nousPunts); // Guardamos los puntos en el almacenamiento local para que puedan ser accedidos en la página de resultados.
+      navigate('/resultat'); // Navegamos a la página de resultados utilizando el hook 'useNavigate'.
+    }
+  }
+
   return (
-    <div>Questionari</div>
+    <div>
+      <p>Pregunta {indexPregunta + 1} de {preguntes.length}</p>
+      <h2>{preguntaActual.pregunta}</h2>
+      
+      {preguntaActual.opcions.map((opcio) => (
+        <button key={opcio} onClick={() => seleccionarOpcio(opcio)}>
+          {opcio}
+        </button>
+      ))}
+
+      <p>Punts: {punts}</p>
+    </div>
   )
 }
