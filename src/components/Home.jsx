b33bcee34df991bd3159ce8ca45b7fe4e0dbe28a
1DAMSCojita <scojita20207@iesjoanramis.org>
2026-02-17 18:49:47 +0100

    Resultado final.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,8 @@ export default function Home() {
 
         <h2>Benvingut als minijocs de la botiga!</h2>
         <p>Posa a prova els teus coneixements de català i videojocs per obtenir descomptes únics!</p>
-
+        <p>Tria un dels minijocs disponibles:</p>
+        
         <Link to="/joc-paraules">Joc de Paraules</Link>
         <Link to="/questionari">Questionari</Link>
     </div>
--- src/components/ResultatFinal.jsx
+++ src/components/ResultatFinal.jsx
@@ -1,6 +1,51 @@
 
+import Header from "./Header"
+
 export default function ResultatFinal() {
+
+  // Declaramos una variable donde almacenamos los puntos obtenidos, recuperándolos del localStorage. Si no hay puntos almacenados, asignaremos un valor de 0.
+  const punts = Number(localStorage.getItem('punts')) || 0
+
+  // Creamos una función para generar un mensaje personalizado según la cantidad de puntos obtenidos:
+  function getMissatge() {
+    // Estructura de control 'if'.
+    // Si los puntos son 9 o más, se muestra un mensaje de felicitación y se indica que se aplicará un descuento del 10% en la próxima compra.
+    // Si los puntos son entre 5 y 8, se muestra un mensaje de buen resultado y se indica que se aplicará un descuento del 5% en la próxima compra.
+    // Si los puntos son menos de 5, se muestra un mensaje de ánimo para mejorar y se invita a intentar nuevamente en el futuro para obtener un descuento.
+    if (punts >= 9) {
+      return "Excel·lent! S'aplicará un descompte del 10% a la teva pròxima compra!"
+    } else if (punts >= 5) {
+      return "Bon resultat! S'aplicará un descompte del 5% a la teva pròxima compra."
+    } else {
+      return "Millora molt! Torna-ho a intentar en el futur per obtendre un descompte."
+    }
+  }
+
   return (
-    <div>ResultatFinal</div>
+    <div>
+      <Header />
+      <h1>Puntuació</h1>
+      <h2>Taula de premis:</h2>
+      <table>
+        <tr>
+          <th>Punts</th>
+          <th>Premis</th>
+        </tr>
+        <tr>
+          <td>De 0 a 4</td>
+          <td>No hi ha premi</td>
+        </tr>
+        <tr>
+          <td>De 5 a 8</td>
+          <td>Descompte del 5%</td>
+        </tr>
+        <tr>
+          <td>De 9 a 10</td>
+          <td>Descompte del 10%</td>
+        </tr>
+      </table>
+      <p>Punts obtinguts: {punts}</p>
+      <p>{getMissatge()}</p>
+    </div>
   )
 }
