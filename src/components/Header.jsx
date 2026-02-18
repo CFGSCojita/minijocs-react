429c00e4680ff709bdfee59ef0dc010998fdb023
1DAMSCojita <scojita20207@iesjoanramis.org>
2026-02-18 12:16:22 +0100

    Estilo al Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,22 @@
-// Importamos 'Link' de React Router:
+// Importamos 'Link' de React Router, el fichero CSS y el logo:
 import { Link } from "react-router-dom";
+import "./Header.css";
+import logo from "../assets/img/logo-black-removebg-preview.webp";
 
 export default function Header() {
-  return (
-    <header>
-        <h1>PixelGame Shop - Minijocs</h1>
-        <nav>
-          <Link to="/">Inici</Link>
-        </nav>
-    </header>
-  )
+    return (
+        <header>
+            <a href="https://remotehost.es/student006/shop">
+                <img
+                    src={logo}
+                    alt="PixelGame Shop logo"
+                    className="header-logo"
+                />
+            </a>
+            <h1>PixelGame Shop - Minijocs</h1>
+            <nav>
+                <Link to="/">Inici</Link>
+            </nav>
+        </header>
+    );
 }
