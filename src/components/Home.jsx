54bb3f64174f8d23ad9fd94ce8db41e316c2ffda
1DAMSCojita <scojita20207@iesjoanramis.org>
2026-02-18 12:46:18 +0100

    Estilo al Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,30 @@
-import { Link } from "react-router-dom"
+// Importamos el 'useNavigate' de React Router, el Header y el fichero CSS:
+import { useNavigate } from "react-router-dom"
 import Header from "./Header"
+import './Home.css'
 
 export default function Home() {
-  return (
-    <div>
-        <Header />
 
+  const navigate = useNavigate(); // Declaramos un hook de navegación.
+
+  return (
+    <div className="home-contenedor">
+      <Header />
+
+      <main className="home-main">
         <h2>Benvingut als minijocs de la botiga!</h2>
         <p>Posa a prova els teus coneixements de català i videojocs per obtenir descomptes únics!</p>
         <p>Tria un dels minijocs disponibles:</p>
-        
-        <Link to="/joc-paraules">Joc de Paraules</Link>
-        <Link to="/questionari">Questionari</Link>
+
+        <div className="jocs-grid">
+          <button className="joc-card" onClick={() => navigate('/joc-paraules')}>
+            Joc de Paraules
+          </button>
+          <button className="joc-card" onClick={() => navigate('/questionari')}>
+            Questionari
+          </button>
+        </div>
+      </main>
     </div>
   )
 }
